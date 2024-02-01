e3ea176b92d71e3aafbf11b9852de53ae0ab4b66
CaolanCode <cmacmahon3@gmail.com>
2024-02-01 21:26:04 +0000

    add division button

--- app/src/main/java/com/caolancode/calculator/view/components/NumberPanel.kt
+++ app/src/main/java/com/caolancode/calculator/view/components/NumberPanel.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,6 +35,15 @@ fun NumberPanel() {
                     contentDescription = stringResource(id = R.string.backspace_description)
                 )
             }
+            Button(
+                onClick = {
+                
+                }
+            ) {
+                Text(text = "C")
+            }
+            CalculatorButton(content = "÷")
         }
+        
     }
 }

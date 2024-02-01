bd443649a50c7a1c234cebcc9dc160e55c45ca22
CaolanCode <cmacmahon3@gmail.com>
2024-02-01 21:21:42 +0000

    add backspace button

--- app/src/main/java/com/caolancode/calculator/view/components/NumberPanel.kt
+++ app/src/main/java/com/caolancode/calculator/view/components/NumberPanel.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.caolancode.calculator.R
 
@@ -22,6 +23,16 @@ fun NumberPanel() {
                 Icon(
                     Icons.Default.Settings,
                     contentDescription = stringResource(id = R.string.settings_description))
+            }
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_backspace_24),
+                    contentDescription = stringResource(id = R.string.backspace_description)
+                )
             }
         }
     }

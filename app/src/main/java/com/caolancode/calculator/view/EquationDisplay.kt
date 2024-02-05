954c2466e311473dda16f1489809f2663aa39f39
CaolanCode <cmacmahon3@gmail.com>
2024-02-05 16:23:38 +0000

    commit styling for result numbers

--- app/src/main/java/com/caolancode/calculator/view/EquationDisplay.kt
+++ app/src/main/java/com/caolancode/calculator/view/EquationDisplay.kt
@@ -3,18 +3,28 @@ package com.caolancode.calculator.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.caolancode.calculator.R
 
 @Composable
 fun EquationDisplay() {
     Column(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.equation_padding)),
         horizontalAlignment = Alignment.End
     ) {
-        Text(text = "0")
+        Text(
+            text = "0",
+            fontSize = dimensionResource(id = R.dimen.equation_font_size).value.sp
+        )
     }
 }
